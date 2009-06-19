79ce9412d80188d634837d68271cc88f0752bf6e
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@4a047dcc-5cd5-11de-bc6d-35d2ebd5fd4e>
2009-06-19 14:39:14 +0000

    [project @ halfdan@halfdans.net-20080906220324-8ycvm8ll9zl5ubfs] Add test to check the the EscapeCharacterDecoder actually outputs commands.
    Original author: Rasmus Toftdahl Olesen <halfdan@halfdans.net>
    Date: 2008-09-07 00:03:24.651000+02:00

--- trunk/tests/TestEscapeCharacterDecoder.cs
+++ trunk/tests/TestEscapeCharacterDecoder.cs
@@ -10,18 +10,33 @@ namespace libVT100.Tests
     [TestFixture]
     public class TestEscapeCharacterDecoder : EscapeCharacterDecoder
     {
+        private struct Command
+        {
+            public byte m_command;
+            public string m_parameter;
+            
+            public Command ( byte _command, string _parameter )
+            {
+                m_command = _command;
+                m_parameter = _parameter;
+            }
+        }
+        
         private List<char[]> m_chars;
+        private List<Command> m_commands;
         
         [SetUp]
         public void SetUp ()
         {
             m_chars = new List<char[]>();
+            m_commands = new List<Command>();
         }
         
         [TearDown]
         public void TearDown ()
         {
             m_chars = null;
+            m_commands = null;
         }
         
         [Test]
@@ -66,6 +81,26 @@ namespace libVT100.Tests
             Assert.AreEqual ( "AA", ReceivedCharacters );
         }
         
+        [Test]
+        public void TestCommands ()
+        {
+            Input ( "A\x001B123m\x001B[123mA" );
+            AssertCommand ( 'm', "123" );
+            AssertCommand ( 'm', "123" );
+            
+            Input ( "A\x001B[123;321;456a\x001B[\"This string is part of the command\"123bA" );
+            AssertCommand ( 'a', "123;321;456" );
+            AssertCommand ( 'b', "\"This string is part of the command\"123" );
+        }
+        
+        private void AssertCommand ( char _command, string _parameter )
+        {
+            Assert.IsNotEmpty ( m_commands );
+            Assert.AreEqual ( (byte) _command, m_commands[0].m_command );
+            Assert.AreEqual ( _parameter, m_commands[0].m_parameter );
+            m_commands.RemoveAt ( 0 );
+        }
+        
         private void Input ( String _input )
         {
             byte[] data = new byte[_input.Length];
@@ -94,6 +129,7 @@ namespace libVT100.Tests
         
         override protected void ProcessCommand ( byte _command, String _parameter )
         {
+            m_commands.Add ( new Command(_command, _parameter) );
         }
         
         override protected void OnCharacters ( char[] _chars )
