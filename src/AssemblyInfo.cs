60187d54d89d97617ad513b6c945968485a029b6
rasmus.toftdahl.olesen <rasmus.toftdahl.olesen@4a047dcc-5cd5-11de-bc6d-35d2ebd5fd4e>
2009-07-27 19:34:34 +0000

    Add vt100render command line utility, set AssemblyInfo version to 0.1.0.*

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "libvt100" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "A library for interpreting a stream of VT100/ANSI commands." )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "Sequanto A/S" )]
 [assembly: AssemblyProduct( "libvt100" )]
-[assembly: AssemblyCopyright( "Copyright ©  2008" )]
+[assembly: AssemblyCopyright( "Copyright © 2008-2009 Sequanto A/S" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "0.1.0.*" )]
+[assembly: AssemblyFileVersion( "0.1.0.*" )]
